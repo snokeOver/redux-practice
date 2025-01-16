ffb32bf67fc1790669e4ac8a6b952df48f65c7fe
snokeOver <snokeover@gmail.com>
2025-01-16 09:17:00 +0600

    Created the task form

---
+++ src/components/task/AddTaskModal.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { TaskForm } from "./TaskForm";
+
+export function AddTaskModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <TaskForm />
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/task/AddTaskModal";
 import TaskCard from "@/components/task/TaskCard";
 import { useTodoSelector } from "@/hooks/useApp";
 
@@ -5,6 +6,10 @@ const Task = () => {
   const { todos } = useTodoSelector();
   return (
     <div className="max-w-6xl flex flex-col gap-8 mx-auto px-5 min-h-screen">
+      <div className="flex justify-between items-center">
+        <h1>Task</h1>
+        <AddTaskModal />
+      </div>
       {todos.map((todo, index) => (
         <TaskCard key={index} todo={todo} />
       ))}
